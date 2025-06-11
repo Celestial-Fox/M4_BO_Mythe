bfe74e78e0688295b2f731f798d59ce9788fe433
Celestial-Fox <stealevw@gmail.com>
2025-06-11 09:54:56 +0200

    Lavabal0.5

---
+++ Assets/Scripts/Enemy/Mechanics/RangedAttacks.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class RangedAttacks : MonoBehaviour
+{
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform bulletPostition;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
