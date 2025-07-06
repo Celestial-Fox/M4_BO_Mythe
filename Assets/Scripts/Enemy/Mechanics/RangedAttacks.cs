9347b891bf6c8e65d171411eb30ceefb3817b6b2
ApollyonLeyssius <38302@ma-web.nl>
2025-07-06 21:11:10 +0200

    update

--- Assets/Scripts/Enemy/Mechanics/RangedAttacks.cs
+++ Assets/Scripts/Enemy/Mechanics/RangedAttacks.cs
@@ -10,6 +10,23 @@ public class RangedAttacks : MonoBehaviour
 
     private float cooldown = 2;
     private float shootTime;
+    [Space]
+    public GameObject enemy1;
+    public GameObject enemy2;
+    public GameObject enemy3;
+    public GameObject enemy4;
+    public GameObject enemy5;
+    public GameObject enemy6;
+    public GameObject enemy7;
+    public GameObject enemy8;
+    public GameObject enemy9;
+    public GameObject enemy10;
+    public GameObject enemy11;
+    public GameObject enemy12;
+    public GameObject enemy13;
+    public GameObject enemy14;
+    public GameObject enemy15;
+    public GameObject enemy16;
     void Start()
     {
         
@@ -27,14 +44,48 @@ public class RangedAttacks : MonoBehaviour
             shoot();
         }
 
-        if (health <= 200)
+        if (health <= 180 && enemy1 != null)
+            enemy1.SetActive(true);
+
+        if (health <= 160 && enemy2 != null)
+            enemy2.SetActive(true);
+
+        if (health <= 140 && enemy3 != null)
+            enemy3.SetActive(true);
+
+        if (health <= 120)
         {
-            Spawner.startTimeBtwSpawn = 30;
+            if (enemy4 != null) enemy4.SetActive(true);
+            if (enemy5 != null) enemy5.SetActive(true);
         }
 
-        if (health <= 180)
+        if (health <= 100)
         {
-            Spawner.startTimeBtwSpawn = 3;
+            if (enemy6 != null) enemy6.SetActive(true);
+            if (enemy7 != null) enemy7.SetActive(true);
+            if (enemy8 != null) enemy8.SetActive(true);
+        }
+
+        if (health <= 80)
+        {
+            if (enemy9 != null) enemy9.SetActive(true);
+            if (enemy10 != null) enemy10.SetActive(true);
+        }
+
+        if (health <= 60)
+        {
+            if (enemy11 != null) enemy11.SetActive(true);
+            if (enemy12 != null) enemy12.SetActive(true);
+        }
+
+        if (health <= 40 && enemy13 != null)
+            enemy13.SetActive(true);
+
+        if (health <= 20)
+        {
+            if (enemy14 != null) enemy14.SetActive(true);
+            if (enemy15 != null) enemy15.SetActive(true);
+            if (enemy16 != null) enemy16.SetActive(true);
         }
 
         if (health == 100)
@@ -44,7 +95,6 @@ public class RangedAttacks : MonoBehaviour
 
         if (health <= 50)
         {   
-            Spawner.startTimeBtwSpawn = 2f;
             cooldown = 0.5f;
         }
     }
